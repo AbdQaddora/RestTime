bcc1401aa27fbeac966f6ca50b983eb2f88dc06e
AbdQaddora <abdqaddora732001@outlook.com>
2021-04-10 15:00:07 +0300

    number falidtion

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
+// var
 var option = {
     animation: true,
-    delay: 5000
+    delay: 3000
 };
+var toastHtmlElemant = document.getElementById("toastBox");
+var toastElemant = new bootstrap.Toast(toastHtmlElemant, option);
 
+
+// show and hide toast
 function toastShow() {
-    var toastHtmlElemant = document.getElementById("toastBox");
-    var toastElemant = new bootstrap.Toast(toastHtmlElemant, option);
     toastElemant.show();
 }
+function toastHide(){
+    toastElemant.hide();
+}
+
+
+// form submit
 
 var my_form = document.getElementById("my_form"), button = document.getElementById("my_button");
 my_form.onsubmit = function () {
     return false;
 }
+
+
+
 var number = document.getElementById("floatingInput"), check = document.getElementById("flexCheckDefault"), select = document.getElementById("select");
-
-
 button.onclick = function () {
+    toastHide();
     if (number.value != "" && select.value != "مكان الإقامة الحالي" && check.checked) {
         if (!isNaN(number.value)) {
             document.querySelector(".toast-body").innerHTML = "تم تأكيد حجزك بنجاح";
